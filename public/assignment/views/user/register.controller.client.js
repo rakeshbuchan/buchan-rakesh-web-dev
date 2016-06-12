0f045a068530839466609b88b2a38472babf2aeb
rakeshbk <rakeshbk@ccs.neu.edu>
2016-06-12 15:52:01 -0400

    register page validation

--- public/assignment/views/user/register.controller.client.js
+++ public/assignment/views/user/register.controller.client.js
@@ -9,22 +9,26 @@
         vm.register = register;
         
         function register(username, password, passwordVerify){
-            if(username != null && password != null && password === passwordVerify) {
-                var user = {"username": username, "password": password};
-                UserService
-                    .createUser(user)
-                    .then(
-                    function(response){
-                        var user = response.data;
-                        if (user) {
-                            $location.url("/user/" + user._id);
-                        } else {
-                            vm.error = "Unable to Register. Try a different Username";
-                        }
-                    }
-                    )
-            }else{
-                vm.error = "Please provide all three inputs and ensure that both passwords entered are same";
+            if(username != null && password != null && passwordVerify != null) {
+                if(password == passwordVerify){
+                    var user = {"username": username, "password": password};
+                    UserService
+                        .createUser(user)
+                        .then(
+                            function(response){
+                                var user = response.data;
+                                if (user) {
+                                    $location.url("/user/" + user._id);
+                                } else {
+                                    vm.error = "Unable to Register. Try a different Username";
+                                }
+                            }
+                        )
+                }
+                else{
+                    vm.error = "Both passwords must be same";
+                }
+
             }
         }
     }
