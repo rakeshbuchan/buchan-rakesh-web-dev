88332143d2d712ae2283d978584febbf6d6895d3
rakeshbk <rakeshbk@ccs.neu.edu>
2016-06-19 20:26:54 -0400

    corrected indentation

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
